70b3f1f2b13779778ac09f0d937ba85d8a449059
prabhushan <priyan.prabhu@gmail.com>
2014-08-10 17:32:18 -0400

    Sync
    Options

--- Chrome Extension/select_links.js
+++ Chrome Extension/select_links.js
@@ -1,6 +1,9 @@
 var links = new Array();
 
 debugger;
+ chrome.storage.sync.get("value", function(data) {
+      console.log("data", data);
+    });
 $("a[href*='.jpg'],[download],[href*='.pdf'],[href*='.docx'],[href*='.zip'],[href*='.xls'],[href*='.xlsx'],[href*='.mp3'],[href*='.mp4']").each(function(index){
 
 links.push({link:this.href,name:this.title});
