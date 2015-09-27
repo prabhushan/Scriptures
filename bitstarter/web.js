35047f0e6dd8b50c3bc3891f57e8116261e132a6
prabhushan <priyan.prabhu@gmail.com>
2015-09-27 08:17:09 -0400

    changes

---
+++ bitstarter/web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html','utf8'));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
